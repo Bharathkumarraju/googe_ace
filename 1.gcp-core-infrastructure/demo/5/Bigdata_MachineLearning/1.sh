22ae49c975b28234a9852ce20cc3b7703f2c47ae
Bharath Dasaraju <bharathkumarraju_dasaraju@ntucenterprise.sg>
2020-09-20 09:47:29 +0800

    Bigdata and ML

---
+++ 1.gcp-core-infrastructure/demo/5/Bigdata_MachineLearning/1.sh
@@ -0,0 +1,14 @@
+Cloud Dataproc:
+  Managed Hadoop MapReduce, Spark, Pig and Hive service
+
+Cloud Dataflow:
+ Stream and batch processing, unified and simplified pipelines.
+
+BigQuery:
+ Analytics Database, Stream data at 100k rows per second
+
+Cloud Pub/Sub:
+  Scalable and flexible enterprise messaging
+
+Cloud Datalab:
+  Interactive Data exploration
